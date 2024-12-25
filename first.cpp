04700cc35714b1262c8171bc86c866805f10356a
MantisA-Z <160739281+MantisA-Z@users.noreply.github.com>
2024-12-25 22:00:47 +0530

    First commit

---
+++ first.cpp
@@ -0,0 +1,13 @@
+// First c++ program
+
+#include <iostream>
+#include <string>
+
+typedef std::string str_t;
+
+int main()
+{
+    str_t name = "Mantis_ji";
+    std::cout << name;
+    return 0;
+}
